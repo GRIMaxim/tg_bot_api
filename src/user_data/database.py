0785e3365c30c1fc996c58b9d75f8d4cecf7f7b8
Grigoruk <grimax14@gmail.com>
2023-10-11 08:56:20 +0300

    Добавление связи между user_data и user_chat

--- src/user_data/database.py
+++ src/user_data/database.py
@@ -1,7 +1,7 @@
 from datetime import UTC, datetime
 
 from sqlalchemy import TIMESTAMP, BigInteger
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.config import Base
 
@@ -11,7 +11,7 @@ class UserData(Base):
 
     __tablename__ = "user_data"
 
-    user_id: Mapped[int] = mapped_column(__type_pos=BigInteger)
+    user_id: Mapped[int] = mapped_column(__type_pos=BigInteger, nullable=True)
     username: Mapped[str] = mapped_column(nullable=True)
     is_subs_active: Mapped[bool] = mapped_column(default=False)
     create_at: Mapped[datetime] = mapped_column(__type_pos=TIMESTAMP(timezone=True), default=datetime.now(tz=UTC))
@@ -19,3 +19,6 @@ class UserData(Base):
     end_subs_at: Mapped[datetime] = mapped_column(__type_pos=TIMESTAMP(timezone=True), nullable=True)
     is_trial_used: Mapped[bool] = mapped_column(default=False)
     online_search_active: Mapped[bool] = mapped_column(default=False)
+
+    chats: Mapped[list["UserChat"]] = relationship(lazy="selectin")
+    
