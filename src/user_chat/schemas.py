590fe6f71d195c7c5635ea3428b37679f5573907
Grigoruk <grimax14@gmail.com>
2023-10-12 10:21:42 +0300

    Изменение схем запросов/ответов user_chat

--- src/user_chat/schemas.py
+++ src/user_chat/schemas.py
@@ -2,37 +2,21 @@ from pydantic import BaseModel, ConfigDict
 
 
 class ChatCreate(BaseModel):
-    """Схема запроса на добавление чата."""
-    
-    user_id: int
-    chat_name: str
-
-
-class ChatCreateMany(BaseModel):
     """Схема запроса на добавление списка чатов."""
-    
-    chats: list[ChatCreate]
+
+    user_id: int
+    chats: list[str]
 
 
 class ChatRead(ChatCreate):
-    """Схема запроса на получение чата."""
-    
+    """Схема запроса на получение списка чата."""
+
     model_config = ConfigDict(from_attributes=True)
 
 
-class ChatReadAll(BaseModel):
-    """Схема запроса на получение списка чатов."""
-    
-    chats: list[ChatRead]
-
-
 class ChatUpdate(ChatCreate):
-    """Схема запроса на обновление чата."""
+    """Схема запроса на обновление списка чатов."""
 
 
 class ChatDelete(ChatCreate):
-    """Схема запроса на удаление чата."""
-
-
-class ChatDeleteMany(ChatCreateMany):
     """Схема запроса на удаление списка чатов."""
