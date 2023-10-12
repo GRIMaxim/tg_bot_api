51fd66a8b7a685d4cda4a7da09497ea9399f04a8
Grigoruk <grimax14@gmail.com>
2023-10-12 11:21:43 +0300

    Вынесение путей роутера в перечисление

--- src/user_data/constants.py
+++ src/user_data/constants.py
@@ -6,3 +6,13 @@ class ErrorMessages(str, Enum):
 
     USER_IS_ALREADY_EXIST = "User is already exist"
     USER_NOT_FOUND = "User not found"
+
+
+class RouterPaths(str, Enum):
+    """Перечисление путей для роутера user_data."""
+
+    CREATE_USER = "/user/"
+    GET_USER = "/user/"
+    GET_ALL_USER_DATA = "/user/all"
+    GET_ALL_USERS = "/user/all"
+    UPDATE_USER = "/user/"
