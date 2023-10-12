b1f2c4c5434c05a5326b93a69e737a2f9d07437a
Grigoruk <grimax14@gmail.com>
2023-10-12 10:18:28 +0300

    Добавление документации

--- src/utils.py
+++ src/utils.py
@@ -11,18 +11,24 @@ from src.config import async_session_maker
 
 @asynccontextmanager
 async def _get_session() -> AsyncIterator[AsyncSession]:
+    """Получение асинхронных сессий для бд."""
     try:
         async with async_session_maker() as session:
             yield session
-    except Exception as error:
+    except Exception:
         await session.rollback()
-        raise error
+        raise
     finally:
         await session.close()
 
 
 async def async_execute(query: Executable) -> Result[Any]:
-    """Курсор для ввода SQL-запросов."""
+    """Курсор для ввода SQL-запросов.
+
+    **Параметры**
+
+    *query* - запрос на языке SQL, сгенерированый с помощью sqlalchemy.
+    """
     async_session: AbstractAsyncContextManager[AsyncSession] = _get_session()
     async with async_session as session:
         query_result = await session.execute(query)
