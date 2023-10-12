e0223141b97a9150aa91b93b10c5cfbdecbb6f8a
Grigoruk <grimax14@gmail.com>
2023-10-12 10:17:41 +0300

    Добавление docstring

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from .user_data.router import router as user_router
 
 
 def get_app() -> FastAPI:
-    """."""
+    """Фабрика для создания приложения FastAPI."""
     app = FastAPI()
 
     app.include_router(user_router)
