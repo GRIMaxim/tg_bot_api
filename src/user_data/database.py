38938638b33a20c3d4685e67b0332c7f87718394
Grigoruk <grimax14@gmail.com>
2023-10-12 10:22:58 +0300

    Добавление relationship

--- src/user_data/database.py
+++ src/user_data/database.py
@@ -2,8 +2,10 @@ from datetime import UTC, datetime
 
 from sqlalchemy import TIMESTAMP, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.ext.associationproxy import AssociationProxy, association_proxy
 
 from src.config import Base
+from src.user_chat.database import UserChat
 
 
 class UserData(Base):
@@ -11,14 +13,23 @@ class UserData(Base):
 
     __tablename__ = "user_data"
 
-    user_id: Mapped[int] = mapped_column(__type_pos=BigInteger, nullable=True, unique=True)
+    user_id: Mapped[int] = mapped_column(
+        __type_pos=BigInteger, nullable=True, unique=True,
+    )
     username: Mapped[str] = mapped_column(nullable=True)
     is_subs_active: Mapped[bool] = mapped_column(default=False)
-    create_at: Mapped[datetime] = mapped_column(__type_pos=TIMESTAMP(timezone=True), default=datetime.now(tz=UTC))
-    start_subs_at: Mapped[datetime] = mapped_column(__type_pos=TIMESTAMP(timezone=True), nullable=True)
-    end_subs_at: Mapped[datetime] = mapped_column(__type_pos=TIMESTAMP(timezone=True), nullable=True)
+    create_at: Mapped[datetime] = mapped_column(
+        __type_pos=TIMESTAMP(timezone=True), default=datetime.now(tz=UTC),
+    )
+    start_subs_at: Mapped[datetime] = mapped_column(
+        __type_pos=TIMESTAMP(timezone=True), nullable=True,
+    )
+    end_subs_at: Mapped[datetime] = mapped_column(
+        __type_pos=TIMESTAMP(timezone=True), nullable=True,
+    )
     is_trial_used: Mapped[bool] = mapped_column(default=False)
     online_search_active: Mapped[bool] = mapped_column(default=False)
 
-    chats: Mapped[list["UserChat"]] = relationship(lazy="selectin")
-    
+    rs_user_chat: Mapped[list[UserChat]] = relationship(lazy="selectin")
+
+    chats: AssociationProxy[list[str]] = association_proxy("rs_user_chat", "chat_name")
