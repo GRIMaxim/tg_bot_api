2b1eaf6cc826ea2d1626e1e0c88b9153bd4ff52f
Grigoruk Maxim <grimax14@gmail.com>
2023-12-13 18:21:25 +0300

    Определение схем данных для user_word

---
+++ src/user_word/schemas.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class WordCreate(BaseModel):
+    """Промежуточная схема для добавления слова."""
+
+    word: str
+    is_key: bool
+
+
+class WordUpdate(WordCreate):
+    """Промежуточная схема для обновления слова."""
+
+
+class WordCreateMany(BaseModel):
+    """Схема запроса на добавление списка слов."""
+
+    user_id: int
+    words: list[WordCreate] = Field(min_length=1)
+
+
+class WordReadMany(WordCreateMany):
+    """Схема запроса на получение списка слов."""
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class WordDeleteMany(WordCreateMany):
+    """Схема запроса на удаление списка слов."""
