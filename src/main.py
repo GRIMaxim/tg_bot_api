6d8483349e980d75b34121141a47724fe12547c1
Grigoruk Maxim <grimax14@gmail.com>
2024-01-06 11:27:32 +0300

    Добавление роутера user_search_settings к приложению

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from .user_data.router import router as user_router
 from .user_chat.router import router as user_chat_router
 from .user_word.router import router as user_word_router
+from .user_search_settings.router import router as settings_router
 
 
 def get_app() -> FastAPI:
@@ -12,6 +13,7 @@ def get_app() -> FastAPI:
     app.include_router(user_router)
     app.include_router(user_chat_router)
     app.include_router(user_word_router)
+    app.include_router(settings_router)
 
     return app
 
