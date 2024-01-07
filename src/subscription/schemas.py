911ba81797f0b9e78d36aa0a8c4ae9934ec3827b
Grigoruk Maxim <grimax14@gmail.com>
2024-01-07 12:39:06 +0300

    Добавление схем запросов для модуля subscription

---
+++ src/subscription/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class SubscriptionCreate(BaseModel):
+    """Схема запроса для добавления подписки и ее настроек."""
+
+    subscription_name: str | None
+    duration: int | None
+    chat_limit: int | None
+    word_limit: int | None
+    is_visible: bool | None
+
+
+class SubscriptionUpdate(SubscriptionCreate):
+    """Схема запроса для обновления подписки и ее настроек."""
+
+
+class SubscriptionRead(SubscriptionCreate):
+    """Схема для получения подписки и ее настроек."""
+
+    model_config = ConfigDict(from_attributes=True)
