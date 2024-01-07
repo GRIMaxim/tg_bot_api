e8704b6d11e46615863209a52254d73049732223
Grigoruk Maxim <grimax14@gmail.com>
2024-01-07 20:17:41 +0300

    Добавление пути для добавления подписки

--- src/subscription/constants.py
+++ src/subscription/constants.py
@@ -4,7 +4,8 @@ from enum import Enum
 class RouterPaths(str, Enum):
     """Перечисление путей для роутера subscription."""
 
-    GET_SUBSCRIPTION = "/subscription/"
+    CREATE_SUBSCRIPTION = "/subscription/"
+    GET_SUBSCRIPTION = "/subscription/"  # noqa:PIE796
     GET_SUBSCRIPTIONS = "/subscription/all"
     UPDATE_SUBSCRIPTION = "/subscription/"  # noqa:PIE796
     DELETE_SUBSCRIPTIONS = "/subscription/"  # noqa:PIE796
